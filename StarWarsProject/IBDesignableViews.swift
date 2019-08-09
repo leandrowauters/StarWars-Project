9adddd301c5e47d45845140931a0be4dae002c43
John Doe <leandro.wauters@gmail.com>
2019-08-09 11:41:59 -0400

    sorted folders, got rid of Storyboard, created designable views

---
+++ StarWarsProject/IBDesignableViews.swift
@@ -0,0 +1,21 @@
+//
+//  IBDesignableViews.swift
+//  StarWarsProject
+//
+//  Created by Leandro Wauters on 8/9/19.
+//  Copyright © 2019 Leandro Wauters. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class WhiteBorderedButton: UIButton {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.borderWidth = 5
+        layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        layer.cornerRadius = bounds.height / 2
+        clipsToBounds = true
+    }
+}
