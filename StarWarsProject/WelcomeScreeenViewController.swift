055c972d34f8ecc8824bb26c158d738775bda125
John Doe <leandro.wauters@gmail.com>
2019-08-09 11:27:05 -0400

    Added welcome screen and created pototype in Adobe XD

---
+++ StarWarsProject/WelcomeScreeenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeScreeenViewController.swift
+//  StarWarsProject
+//
+//  Created by Leandro Wauters on 8/9/19.
+//  Copyright © 2019 Leandro Wauters. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeScreeenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
